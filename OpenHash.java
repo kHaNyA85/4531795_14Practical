c17b491a3d2ee68602476f2f0c509760d79fc126
kHaNyA85 <4531795@myuwc.ac.za>
2026-03-04 14:46:44 +0200

    Add OpenHash class with Entry inner class

--- OpenHash.java
+++ OpenHash.java
@@ -1,1 +1,17 @@
+public class OPenHash {
+  
+  private static class Entry{
+    String key;
+    String value;
+    Entry(String k, String v) {
+      key = k;
+      value = v;
+    }
+  }
 
+  private Entry[] table;
+  private int size, m;
+  
+
+  
+}
