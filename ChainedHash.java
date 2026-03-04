efe2c7cc7bb8fb911080afaa351d71658eb31a45
kHaNyA85 <4531795@myuwc.ac.za>
2026-03-04 15:31:15 +0200

    Created class ChainedHash

--- ChainedHash.java
+++ ChainedHash.java
@@ -1,1 +1,12 @@
-
+public class ChainedHash{
+  private static class Node{
+    String key, value;
+    Node next;
+    Node(String k, String v) {
+      key = k;
+      value = v;
+      next = null;
+    }
+    
+  }
+}
