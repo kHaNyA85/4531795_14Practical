3f2a7706991ca16fbc538b868ffab41a8c2d4de4
kHaNyA85 <4531795@myuwc.ac.za>
2026-03-04 14:50:24 +0200

    Add constructor and hash function to OpenHash

--- OpenHash.java
+++ OpenHash.java
@@ -11,7 +11,17 @@ public class OPenHash {
 
   private Entry[] table;
   private int size, m;
-  
 
+  public OpenHash(int m) {
+    this.m = m;
+    table = new Entry[m + 1];
+    size = 0;
+  }
+
+  private int hash(String key) {
+    int x = Integer.parseInt(key);
+    return (x % m) + 1;
+  }
+  
   
 }
