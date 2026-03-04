eadebbaa33ec968dacf84a2e1c4a17aa0ba0c8fa
kHaNyA85 <4531795@myuwc.ac.za>
2026-03-04 16:22:27 +0200

    Created Class

--- Hashing.java
+++ Hashing.java
@@ -1,1 +1,19 @@
+import java.util.*;
 
+public class Main {
+
+    static final int N = 1 << 20;
+    static final int REPETITIONS = 30;
+
+    static String[][] generateData() {
+        String[] keys = new String[N];
+        for (int i = 0; i < N; i++) keys[i] = "key" + i;
+        List<String> list = Arrays.asList(keys);
+        Collections.shuffle(list);
+        String[][] data = new String[N][2];
+        for (int i = 0; i < N; i++) {
+            data[i][0] = list.get(i);
+            data[i][1] = String.valueOf(i + 1);
+        }
+        return data;
+    }
